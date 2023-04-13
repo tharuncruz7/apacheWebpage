a7ac43466653ef235c9ebfcad61c03bb73f033e1
devopskroy <109326965+devopskroy@users.noreply.github.com>
2023-04-13 09:31:23 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <pre>
 <?php
 echo "<body style='background-color:orange'>";
-print "welcome to webserver Create by Terraform!!!!<br />";
+print "welcome to webserver Created by Terraform!!!!<br />";
 print `/usr/sbin/ifconfig`;
 ?>
 </pre>
